85e3ab4325ec1bb79caf2deb9442a3d4f7605e32
axu732 <axu732@aucklanduni.ac.nz>
2024-01-22 22:21:31 +1300

    Added state hook for taskForm class

--- src/TaskForm.js
+++ src/TaskForm.js
@@ -1,8 +1,13 @@
+import { useState } from "react";
+
 export default function TaskForm(){
+  const [taskName, setTaskName] = useState("");
   return (
     <form>
         <button>+</button>
-        <input type="text"/>
+        <input type="text" value = {taskName}
+        onChange={(event => setTaskName(event.target.value))} 
+        placeholder="Next Task?"/>
       </form>
   )
 }
