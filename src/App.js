e5703cae5a92327cb9127caabc184048dc45ddb5
axu732 <axu732@aucklanduni.ac.nz>
2024-01-25 14:03:03 +1300

    Added local storage for storing the task state

--- src/App.js
+++ src/App.js
@@ -1,27 +1,39 @@
-import { useState } from 'react';
-import './App.css';
-import Task from './Task';
-import TaskForm from './TaskForm';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Task from "./Task";
+import TaskForm from "./TaskForm";
 
 function App() {
   const [tasks, setTasks] = useState([]);
 
+  // This will set the tasks array into local storage. It will check if it is 0 and return if it is, making sure it is not set back to an empty array
+  useEffect(() => {
+    if(tasks.length === 0){
+      return;
+    }
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
+  // We will then read from local storage and setTasks to the tasks array
+  useEffect(() => {
+    const tasksArray = JSON.parse(localStorage.getItem('tasks'));
+    setTasks(tasksArray);
+  }, []);
+
   // function that that will add the task to the end of an array
   function addTask(taskName) {
-    setTasks(prev => {
-      return [...prev, {name:taskName, done:false}];
+    setTasks((prev) => {
+      return [...prev, { name: taskName, done: false }];
     });
-
   }
 
   // Defines the onAdd function for the TaskForm component and then loops through the tasks array to display them
   return (
     <main>
-      <TaskForm onAdd = {addTask}/>
-      {tasks.map(task => (
-        <Task {...task}/>
+      <TaskForm onAdd={addTask} />
+      {tasks.map((task) => (
+        <Task {...task} />
       ))}
-
     </main>
   );
 }
