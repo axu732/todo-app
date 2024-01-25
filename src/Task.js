3ab0b1d40652de4250690e095472867233d13fd9
axu732 <axu732@aucklanduni.ac.nz>
2024-01-25 14:58:32 +1300

    Added transitions to when a task is done

--- src/Task.js
+++ src/Task.js
@@ -5,9 +5,9 @@ export default function Task({name, done, onToggle}) {
   return (
     //Task div has a ternary operator to distinguish done or not done
     // OnClick will run onToggle from App. It is cycling between done and not done
-    <div className={'task' + (done?'done': 'notDone')}>
+    <div className={'task ' + (done?'done': 'notDone')}>
       <Checkbox checked={done} onClick={() => onToggle(!done)} /> 
-        {name}
+        <span>{name}</span>
       </div>
   )
 }
